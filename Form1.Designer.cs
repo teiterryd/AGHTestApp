cf69e58431676396b8c83e32fff1e19c7b1aabf4
terryd <terryd@alumapower.com>
2021-10-30 14:19:49 -0400

    Center Screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -63,6 +63,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDoNotTouch);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GitHub Tester";
             this.ResumeLayout(false);
 
